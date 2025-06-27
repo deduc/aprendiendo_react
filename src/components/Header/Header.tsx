c57dc84b08ff0850e099c0ab21c77e92bd04ebc2
PORTATIL-IVAN\deduc <deductivegoose@outlook.es>
2025-06-27 19:23:53 +0200

    comenzar app

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import "./Header.css";
+
+export function Header() {
+    return (
+        <>
+            <header>
+                <article>
+                    <h1>Aprendiendo React</h1>
+                    <p>Para sorprender a la peña</p>
+                </article>
+
+                <article>
+                    <p>Por y para programadores</p>
+                </article>
+
+                <article>
+                    <button>Iniciar sesión</button>
+                    <button>Registrarme</button>
+                </article>
+            </header>
+        </>
+    )
+}
